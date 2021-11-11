f5ca2168ac5890c8bb272d29f3c77a2e00657449
RLoo <jonathanfaren@gmail.com>
2021-11-11 21:44:43 +0700

    page routing

--- lib/screen/account.dart
+++ lib/screen/account.dart
@@ -1,7 +1,9 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:masakin_app/navbar_key.dart';
 
 class AccountScreen extends StatefulWidget {
-  const AccountScreen({ Key? key }) : super(key: key);
+  const AccountScreen({Key? key}) : super(key: key);
 
   @override
   _AccountScreenState createState() => _AccountScreenState();
@@ -11,7 +13,7 @@ class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
-    );
+     child: Center(child: Text('acccount')),
+       );
   }
 }
