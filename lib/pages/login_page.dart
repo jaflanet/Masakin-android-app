e0c839caa2fa591ae88f0dbe2f62960e5db3c343
pademss <fatma.putri@ui.ac.id>
2021-11-18 22:37:29 +0700

    login signup sementara

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:masakin_app/utility/validator.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class loginPage extends StatefulWidget {
   // const loginPage({ Key? key }) : super(key: key);
@@ -11,58 +12,220 @@ class loginPage extends StatefulWidget {
 class _loginPageState extends State<loginPage> {
   String? _email, _password;
 
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  void validateInput() {
+    if (formkey.currentState!.validate()) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      print("Not validated");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var doLogin = () {};
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(40.0),
-          child: Form(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 15.0,
-                ),
-                const Text("Email"),
-                const SizedBox(
-                  height: 5.0,
-                ),
-                TextFormField(
-                  autofocus: false,
-                  // validator: validateEmail(value),
-                  onSaved: (value) => _email = value,
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text("Password"),
-                const SizedBox(
-                  height: 5.0,
-                ),
-                TextFormField(
-                  autofocus: false,
-                  // validator: validateEmail(value),
-                  onSaved: (value) => _password = value,
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                TextButton(onPressed: (){Navigator.pushReplacementNamed(context, '/home');}, child: const Text("login")),
-                TextButton(
-                  child: const Text("register"), 
-                onPressed: (){
-                  Navigator.pushReplacementNamed(context, '/register');
-                },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+        resizeToAvoidBottomInset: false,
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/YUK.png'),
+                    fit: BoxFit.cover)),
+            child: Form(
+                autovalidateMode: AutovalidateMode.always,
+                key: formkey,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      child: Image.asset(
+                        "assets/images/login.png",
+                        scale: 1.5,
+                      ),
+                    ),
+                    Positioned(
+                      top: 15,
+                      left: 15,
+                      child: Image.asset(
+                        "assets/images/splash.png",
+                        scale: 13,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child:
+                          Image.asset("assets/images/login_2.png", scale: 1.3),
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(left: 75.0, right: 75.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Log in',
+                              style: TextStyle(
+                                  fontSize: 28,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            const SizedBox(height: 25),
+                            buildEmail(),
+                            const SizedBox(height: 23.0),
+                            buildPassword(),
+                            const SizedBox(height: 29),
+                            buildButtonLogin(),
+                            const SizedBox(height: 40),
+                            const Text('Didnt have account?',
+                                style: TextStyle(fontFamily: 'Montserrat')),
+                            const SizedBox(height: 10),
+                            buildButtonSignUp(),
+                          ],
+                        ))
+                  ],
+                )
+                // child: Container(
+                //     padding: EdgeInsets.only(left: 90.0, right: 90.0),
+                //     child: Column(
+                //       children: [
+                //         const Text('Login'),
+                //         buildEmail(),
+                //         const SizedBox(height: 23.0),
+                //         buildPassword(),
+                //         const SizedBox(height: 23),
+                //         buildButtonLogin(),
+                //         const SizedBox(height: 25),
+                //         const Text('Didnt have account?'),
+                //         const SizedBox(height: 10),
+                //         buildButtonSignUp(),
+                //       ],
+                //     ))
+                // appBar: AppBar(
+                //   title: Text('Login'),
+                // ),
+                // body: SingleChildScrollView(
+                //   child: Container(
+                //     padding: const EdgeInsets.all(40.0),
+                //     child: Form(
+                //       child: Column(
+                //         children: [
+                //           const SizedBox(
+                //             height: 15.0,
+                //           ),
+                //           const Text("Email"),
+                //           const SizedBox(
+                //             height: 5.0,
+                //           ),
+                //           TextFormField(
+                //             autofocus: false,
+                //             // validator: validateEmail(value),
+                //             onSaved: (value) => _email = value,
+                //           ),
+                //           const SizedBox(
+                //             height: 20.0,
+                //           ),
+                //           const Text("Password"),
+                //           const SizedBox(
+                //             height: 5.0,
+                //           ),
+                //           TextFormField(
+                //             autofocus: false,
+                //             // validator: validateEmail(value),
+                //             onSaved: (value) => _password = value,
+                //           ),
+                //           const SizedBox(
+                //             height: 20.0,
+                //           ),
+                //           TextButton(
+                //               onPressed: () {
+                //                 Navigator.pushReplacementNamed(context, '/home');
+                //               },
+                //               child: const Text("login")),
+                //           TextButton(
+                //             child: const Text("register"),
+                //             onPressed: () {
+                //               Navigator.pushReplacementNamed(context, '/register');
+                //             },
+                //           ),
+                //         ],
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                )));
   }
+
+  Widget buildEmail() => Container(
+        child: Container(
+            padding: EdgeInsets.only(left: 16, right: 16),
+            child: TextFormField(
+              validator: RequiredValidator(errorText: "Required"),
+              textInputAction: TextInputAction.done,
+              decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(
+                      color: Color(0xFF817E7E),
+                      fontSize: 16,
+                      fontFamily: 'Montserrat'),
+                  border: InputBorder.none),
+            )),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(37),
+            color: Colors.white,
+            border: Border.all(color: Color(0xFFF5C901), width: 2),
+            boxShadow: kElevationToShadow[6]),
+      );
+
+  Widget buildPassword() => Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(37),
+          color: Colors.white,
+          border: Border.all(color: Color(0xFFF5C901), width: 2),
+          boxShadow: kElevationToShadow[6]),
+      child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: TextFormField(
+            obscureText: true,
+            validator: RequiredValidator(errorText: "Required"),
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(
+                    color: Color(0xFF817E7E),
+                    fontSize: 16,
+                    fontFamily: 'Montserrat'),
+                border: InputBorder.none),
+          )));
+
+  Widget buildButtonLogin() => TextButton(
+      onPressed: () {
+        validateInput();
+      },
+      child: Text('Log in',
+          style: TextStyle(
+              fontSize: 18, color: Colors.black, fontFamily: 'Montserrat')),
+      style: TextButton.styleFrom(
+        elevation: 6,
+        shadowColor: Colors.black,
+        padding: EdgeInsets.fromLTRB(65.0, 15.0, 65.0, 15.0),
+        backgroundColor: Color(0xFFF5C901),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(37)),
+      ));
+
+  Widget buildButtonSignUp() => TextButton(
+      onPressed: () {
+        Navigator.pushReplacementNamed(context, '/register');
+      },
+      child: Text('Sign up',
+          style: TextStyle(
+              fontSize: 18, color: Colors.white, fontFamily: 'Montserrat')),
+      style: TextButton.styleFrom(
+        elevation: 6,
+        shadowColor: Colors.black,
+        padding: EdgeInsets.fromLTRB(55.0, 15.0, 55.0, 15.0),
+        backgroundColor: Color(0xFFFF8023),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(37)),
+      ));
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class registerPage extends StatefulWidget {
   const registerPage({Key? key}) : super(key: key);
@@ -8,63 +9,236 @@ class registerPage extends StatefulWidget {
 }
 
 class _registerPageState extends State<registerPage> {
-  // final formKey = GlobalKey<FormFieldState>();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   String? _email, _password;
+
+  void validateInput() {
+    if (formKey.currentState!.validate()) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      print("Not validated");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    var doRegister = () {
-      // print('registered');
+    // var doRegister = () {
+    //   // print('registered');
 
-      // final form? = formKey.currentState;
-      // form.save();
-    };
+    //   // final form? = formKey.currentState;
+    //   // form.save();
+    // };
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(40.0),
-          child: Form(
-            // key: formKey,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 15.0,
-                ),
-                const Text("Email"),
-                const SizedBox(
-                  height: 5.0,
-                ),
-                TextFormField(
-                  autofocus: false,
-                  // validator: validateEmail(value),
-                  onSaved: (value) => _email = value,
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text("Password"),
-                const SizedBox(
-                  height: 5.0,
-                ),
-                TextFormField(
-                  autofocus: false,
-                  // validator: validateEmail(value),
-                  onSaved: (value) => _password = value,
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                TextButton(
-                    onPressed: doRegister, child: const Text("register")),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+        
+        // appBar: AppBar(
+        //   title: Text('Register'),
+        // ),
+        body: new SingleChildScrollView(
+            child: Container(
+                child: Column(
+          children: <Widget>[
+            Align(
+                alignment: Alignment.topRight,
+                child: Image.asset(
+                  "assets/images/register.png",
+                  scale: 1.3,
+                )),
+            Container(
+                child: Container(
+                    padding: EdgeInsets.only(left: 75.0, right: 75.0),
+                    child: Form(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        key: formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(height: 17),
+                            const Text(
+                              'Sign up',
+                              style: TextStyle(
+                                  fontSize: 28,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            const SizedBox(height: 30),
+                            buildName(),
+                            const SizedBox(height: 23.0),
+                            buildEmail(),
+                            const SizedBox(height: 23),
+                            buildPassword(),
+                            const SizedBox(height: 23),
+                            buildPhoneNumber(),
+                            const SizedBox(height: 35),
+                            buildButtonSignUp(),
+                          ],
+                        ))))
+          ],
+        )))
+        // body: SingleChildScrollView(
+        //   child: Stack(children: <Widget>[
+        //     Positioned(
+        //       top: 0,
+        //       right: 0,
+        //       child: Image.asset(
+        //         "assets/images/login.png",
+        //         scale: 1.5,
+        //       ),
+        //     ),
+        //     Positioned(
+        //       top: 15,
+        //       right: 15,
+        //       child: Image.asset(
+        //         "assets/images/splash.png",
+        //         scale: 13,
+        //       ),
+        //     ),
+        //   ]
+        // child: Container(
+        //   padding: const EdgeInsets.all(40.0),
+        //   child: Form(
+        //     // key: formKey,
+        //     child: Column(
+        //       children: [
+        //         const SizedBox(
+        //           height: 15.0,
+        //         ),
+        //         const Text("Email"),
+        //         const SizedBox(
+        //           height: 5.0,
+        //         ),
+        //         TextFormField(
+        //           autofocus: false,
+        //           // validator: validateEmail(value),
+        //           onSaved: (value) => _email = value,
+        //         ),
+        //         const SizedBox(
+        //           height: 20.0,
+        //         ),
+        //         const Text("Password"),
+        //         const SizedBox(
+        //           height: 5.0,
+        //         ),
+        //         TextFormField(
+        //           autofocus: false,
+        //           // validator: validateEmail(value),
+        //           onSaved: (value) => _password = value,
+        //         ),
+        //         const SizedBox(
+        //           height: 20.0,
+        //         ),
+        //         TextButton(
+        //             onPressed: doRegister, child: const Text("register")),
+        //       ],
+        //     ),
+        //   ),
+        // ),
+        );
+    // ));
   }
+
+  Widget buildName() => Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(37),
+          color: Colors.white,
+          border: Border.all(color: Color(0xFFF5C901), width: 2),
+          boxShadow: kElevationToShadow[6]),
+      child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: TextFormField(
+            validator: RequiredValidator(errorText: "Required"),
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+                hintText: 'Name',
+                hintStyle: TextStyle(
+                    color: Color(0xFF817E7E),
+                    fontSize: 16,
+                    fontFamily: 'Montserrat'),
+                border: InputBorder.none),
+          )));
+
+  Widget buildEmail() => Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(37),
+          color: Colors.white,
+          border: Border.all(color: Color(0xFFF5C901), width: 2),
+          boxShadow: kElevationToShadow[6]),
+      child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: TextFormField(
+            validator: MultiValidator([
+              RequiredValidator(errorText: "Required"),
+              EmailValidator(errorText: "Not a valid email")
+            ]),
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(
+                    color: Color(0xFF817E7E),
+                    fontSize: 16,
+                    fontFamily: 'Montserrat'),
+                border: InputBorder.none),
+          )));
+
+  Widget buildPassword() => Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(37),
+          color: Colors.white,
+          border: Border.all(color: Color(0xFFF5C901), width: 2),
+          boxShadow: kElevationToShadow[6]),
+      child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: TextFormField(
+            obscureText: true,
+            validator: MultiValidator([
+              RequiredValidator(errorText: "Required"),
+              MinLengthValidator(8,
+                  errorText: "Password should be at least 8 characters")
+            ]),
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(
+                    color: Color(0xFF817E7E),
+                    fontSize: 16,
+                    fontFamily: 'Montserrat'),
+                border: InputBorder.none),
+          )));
+
+  Widget buildPhoneNumber() => Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(37),
+          color: Colors.white,
+          border: Border.all(color: Color(0xFFF5C901), width: 2),
+          boxShadow: kElevationToShadow[6]),
+      child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: TextFormField(
+            validator: MultiValidator([
+              RequiredValidator(errorText: "Required"),
+            ]),
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+                hintText: 'Phone Number',
+                hintStyle: TextStyle(
+                    color: Color(0xFF817E7E),
+                    fontSize: 16,
+                    fontFamily: 'Montserrat'),
+                border: InputBorder.none),
+          )));
+
+  Widget buildButtonSignUp() => TextButton(
+      onPressed: () {
+        validateInput();
+      },
+      child: Text('Sign up',
+          style: TextStyle(
+              fontSize: 18, color: Colors.white, fontFamily: 'Montserrat')),
+      style: TextButton.styleFrom(
+        elevation: 6,
+        shadowColor: Colors.black,
+        padding: EdgeInsets.fromLTRB(55.0, 15.0, 55.0, 15.0),
+        backgroundColor: Color(0xFFFF8023),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(37)),
+      ));
 }
