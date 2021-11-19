faadabb91d8df40bde869480ffedbcfb8564e057
Anathapindika Muliawan <anathapindika.muliawan@ui.ac.id>
2021-11-20 00:03:40 +0700

    gajadi ganti restaurant pusing

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -9,6 +9,10 @@ class Restaurant {
   Restaurant(this.name, this.location, this.rating, this.tag);
 
   static Restaurant generateRestaurant() {
-    return Restaurant('Restaurant', 'Jalan Catur Darma No.23A RT 3 RW 6, Cijantung, Pasar Rebo, Jakarta Timur', 4.7, 'halal');
+    return Restaurant(
+        'Restaurant',
+        'Jalan Catur Darma No.23A RT 3 RW 6, Cijantung, Pasar Rebo, Jakarta Timur',
+        4.7,
+        'halal');
   }
 }
