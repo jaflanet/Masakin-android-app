738ff050a1c5d41a3f46d57c70ff2972fe48dfc2
RLoo <jonathanfaren@gmail.com>
2021-11-20 20:34:47 +0700

    admin section

---
+++ lib/pages/adminScreen/addMenu.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class addMenu extends StatelessWidget {
+  const addMenu({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/pages/adminScreen/addRestaurant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class addRestaurant extends StatelessWidget {
+  const addRestaurant({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:
+      TextButton(onPressed: ,) ,
+    );
+  }
+}
