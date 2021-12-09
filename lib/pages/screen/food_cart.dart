667ba4686f1253348a463d04f4a377e3a03217a4
Anathapindika Muliawan <anathapindika.muliawan@ui.ac.id>
2021-12-10 01:28:46 +0700

    food cart almost done

--- lib/pages/screen/food_cart.dart
+++ lib/pages/screen/food_cart.dart
@@ -6,18 +6,60 @@ class foodCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-            backgroundColor: Color(0xFFF5C901), title: Text("Your Cart")),
-        body: SingleChildScrollView(
-          child: Column(
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
             children: [
-              CartProducts(),
-              CartTotal(),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFF5C901),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                  ),
+                ),
+                height: 80,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(35, 25, 0, 20),
+                child: Row(
+                  children: [
+                    Icon(Icons.add_shopping_cart),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Your Cart',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(25, 30, 0, 20),
+            child: Container(
+              child: Text(
+                'Order List',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          CartProducts(),
+          CartTotal(),
+        ],
       ),
     );
   }
